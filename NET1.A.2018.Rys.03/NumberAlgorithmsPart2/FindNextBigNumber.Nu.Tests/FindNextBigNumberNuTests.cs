b4f425839c4e3aec4324592087ae34008e71843f
Rys Vladislav <onesssse@gmail.com>
2018-11-11 02:26:57 +0300

    FindNextBigNum upadated

---
+++ NET1.A.2018.Rys.03/NumberAlgorithmsPart2/FindNextBigNumber.Nu.Tests/FindNextBigNumberNuTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NumberAlgorithmsPart2;
+using NUnit.Framework;
+
+
+namespace FindNextBigNumber.Nu.Tests
+{
+    [TestFixture]
+    public class FindNextBigNumberNuTests
+    {
+        [TestCase(10, -1)]
+        [TestCase(20, -1)]
+        [TestCase(12, 21)]
+        [TestCase(513, 531)]
+        [TestCase(2017, 2071)]
+        [TestCase(414, 441)]
+        [TestCase(144, 414)]
+        [TestCase(1234321, 1241233)]
+        [TestCase(1234126, 1234162)]
+        [TestCase(3456432, 3462345)]
+        public void FindNextBigNumber_Number_ValidOutPut(int number, int expected)
+        {
+            Assert.AreEqual(expected, FindNextBiggerNumber.FindNextBigNumber(number));
+        }
+
+        [TestCase(-10)]
+        [TestCase(-5123)]
+        public void FindNextBigNumber_Number_ValidOutPut(int number)
+        {
+            Assert.Throws<ArgumentException>(() => FindNextBiggerNumber.FindNextBigNumber(number));
+        }
+
+
+    }
+}
--- NET1.A.2018.Rys.03/NumberAlgorithmsPart2/NumberAlgorithmsPart2/FindNextBiggerNumber.cs
+++ NET1.A.2018.Rys.03/NumberAlgorithmsPart2/NumberAlgorithmsPart2/FindNextBiggerNumber.cs
@@ -4,7 +4,92 @@ using System.Text;
 
 namespace NumberAlgorithmsPart2
 {
-    class FindNextBiggerNumber
+    /// <summary>
+    /// Implementing the Finding Biggest Nest Number algorithm.
+    /// </summary>
+    public static class FindNextBiggerNumber
     {
+        /// <summary>
+        /// Public method that checks inputs and calls algorithm
+        /// if inputs are valid.
+        /// </summary>
+        /// <param name="number"> Input number in range of: [0, Int32.MaxValue] </param>
+        /// <returns> Returns result of the main algorithm method. </returns>
+        public static int FindNextBigNumber(int number)
+        {
+            if (number < 0) { throw new ArgumentException(nameof(number)); }
+            if (number % 10 == 0) return -1;
+            return Find(number);
+        }
+
+        /// <summary>
+        /// The main method that find next biggest number that can be compiled from the
+        /// "number" digits.
+        /// </summary>
+        /// <param name="number">Input number in range of: [0, Int32.MaxValue] </param>
+        /// <returns>Returns the biggest next number. </returns>
+        private static int Find(int number)
+        {
+            var numbers = ConvertToArray(number);
+            for (var i = numbers.Length - 1; i >= 1; i--)
+            {
+                if (numbers[i] <= numbers[i - 1]) continue;
+
+                Swap(ref numbers[i], ref numbers[i - 1]);
+                if (numbers.Length - i <= 1) return ConvertToNum(numbers);
+            
+                var unsortedNUms = new int[numbers.Length - i];
+                Array.Copy(numbers, i, unsortedNUms, 0, numbers.Length - i);
+                Array.Sort(unsortedNUms);
+                Array.Copy(unsortedNUms, 0, numbers, i, unsortedNUms.Length);
+
+                return ConvertToNum(numbers);
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Swap method fo some ref params.
+        /// </summary>
+        /// <param name="a"> First parameter to swap. </param>
+        /// <param name="b"> Second parameter to swap. </param>
+        private static void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        /// <summary>
+        /// Method that take a number as input and returns an array of
+        /// digits of that number.
+        /// </summary>
+        /// <param name="number"> Number that needs to be divided on digits. </param>
+        /// <returns> Returns the arrays of digits. </returns>
+        private static int[] ConvertToArray(int number)
+        {
+            var nums = new int[number.ToString().Length];
+            for (var i = 0; i < nums.Length; i++)
+            {
+                nums[nums.Length - 1 - i] = number % 10;
+                number /= 10;
+            }
+            return nums;
+        }
+
+        /// <summary>
+        /// Method that take an array of digits as input and returns the int value.
+        /// </summary>
+        /// <param name="nums"> The array that needs to be translated into int value. </param>
+        /// <returns> Returns the int value from the arrays of digits. </returns>
+        private static int ConvertToNum(int[] nums)
+        {
+            StringBuilder num = new StringBuilder();
+            foreach (var n in nums)
+            {
+                num.Append(n);
+            }
+            return int.Parse(num.ToString());
+        }
     }
 }
