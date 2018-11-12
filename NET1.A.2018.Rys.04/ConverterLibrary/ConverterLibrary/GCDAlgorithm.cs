91da67ccde36f5c2248c732652f26f71b73f7f0b
Rys Vladislav <onesssse@gmail.com>
2018-11-12 04:03:42 +0300

    GCD StopWatch updated

--- NET1.A.2018.Rys.04/ConverterLibrary/ConverterLibrary/GCDAlgorithm.cs
+++ NET1.A.2018.Rys.04/ConverterLibrary/ConverterLibrary/GCDAlgorithm.cs
@@ -1,10 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace ConverterLibrary
 {
-    class GCDAlgorithm
+    public static class GCDAlgorithm
     {
+        public static int FindGCDEuclid(int firstInput, int secondInput)
+        {
+            int result = InputValidation(Math.Abs(firstInput), Math.Abs(secondInput));
+            if (result == -1)
+            {
+                return GSDEuclid(Math.Abs(firstInput), Math.Abs(secondInput));
+            }
+
+            return result;
+        }
+
+        public static int FindGCDEuclid(int firstInput, int secondInput, ref Stopwatch sw)
+        {
+            sw.Start();
+            var result = FindGCDEuclid(firstInput, secondInput);
+            sw.Stop();
+            return result;
+        }
+
+        public static int FindGCDShtein(int firstInput, int secondInput)
+        {
+            int result = InputValidation(Math.Abs(firstInput), Math.Abs(secondInput));
+            if (result == -1)
+            {
+                return GSDEuclid(Math.Abs(firstInput), Math.Abs(secondInput));
+            }
+
+            return result;
+        }
+
+        public static int FindGCDShtein(int firstInput, int secondInput, ref Stopwatch sw)
+        {
+            sw.Start();
+            var result = FindGCDShtein(firstInput, secondInput);
+            sw.Stop();
+            return result;
+        }
+
+        private static int InputValidation(int firstInput, int secondInput)
+        {
+            if (firstInput == 0) return secondInput;
+            if (secondInput == 0) return firstInput;
+            if (firstInput == secondInput) return firstInput;
+            return -1;
+        }
+
+        private static int GSDEuclid(int first, int second)
+        {
+            while (first != 0)
+            {
+                first = second % (second = first);
+            }
+
+            return second;
+        }
+
+        private static int FindGCDStein(int first, int second)
+        {
+            if (first == 0) return second;
+            if (second == 0) return first;
+            if (first == second) return first;
+
+            var value1IsEven = (first & 1) == 0;
+            var value2IsEven = (second & 1) == 0;
+
+            if (value1IsEven && value2IsEven)
+            {
+                return FindGCDStein(first >> 1, second >> 1) << 1;
+            }
+
+            if (value1IsEven)
+            {
+                return FindGCDStein(first >> 1, second);
+            }
+
+            if (value2IsEven)
+            {
+                return FindGCDStein(first, second >> 1);
+            }
+
+            if (first > second)
+            {
+                return FindGCDStein((first - second) >> 1, second);
+            }
+            else
+            {
+                return FindGCDStein(first, (second - first) >> 1);
+            }
+        }
     }
 }
---
+++ NET1.A.2018.Rys.04/ConverterLibrary/GCDAlgorithms.Nu.Tests/GCDAlgorithmsNuTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using ConverterLibrary;
+using NUnit.Framework;
+
+namespace GCDAlgorithms.Nu.Tests
+{
+    [TestFixture]
+    public class GCDAlgorithmsNuTests
+    {
+        [Test]
+        public void GCDEuclid_TenAndFive_Five()
+        {
+            var expected = 5;
+
+            var actual = GCDAlgorithm.FindGCDEuclid(10, 5);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0, 4, 4)]
+        [TestCase(17, 29, 1)]
+        [TestCase(123, 3, 3)]
+        [TestCase(150, 50, 50)]
+        [TestCase(-100, 80, 20)]
+        [TestCase(152, 50, 2)]
+        [TestCase(0, 0, 0)]
+        public void GCDEuclid_TenAndFive_Five(int first, int second, int expected)
+        {
+            var actual = GCDAlgorithm.FindGCDEuclid(first, second);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0, 4)]
+        [TestCase(17, 29)]
+        [TestCase(123, 3)]
+        [TestCase(150, 50)]
+        [TestCase(-100, 80)]
+        [TestCase(152, 50)]
+        [TestCase(0, 0)]
+        [TestCase(100046300, 10034000)]
+        public void GCD_StopWatchTest(int first, int second)
+        {
+            Stopwatch sw = new Stopwatch();
+            
+
+            sw.Start();
+            GCDAlgorithm.FindGCDEuclid(first, second);
+            sw.Stop();
+            var euclid = sw.Elapsed;
+
+            sw.Reset();
+
+            sw.Start();
+            GCDAlgorithm.FindGCDShtein(first, second);
+            sw.Stop();
+            var shtein = sw.Elapsed;
+
+            Debug.WriteLine($"euclid: {euclid}, shtein: {shtein}");
+            Assert.AreNotSame(euclid, shtein);
+        }
+
+    }
+}
