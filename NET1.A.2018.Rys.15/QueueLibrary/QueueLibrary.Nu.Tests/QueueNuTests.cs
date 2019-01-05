06c610cc160de858caf16eb1c862cd2d9550b57b
wald3 <onessse@gmail.com>
2019-01-05 23:55:30 +0300

    Queue NuTests updated

---
+++ NET1.A.2018.Rys.15/QueueLibrary/QueueLibrary.Nu.Tests/QueueNuTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QueueLibrary.Nu.Tests
+{
+    [TestFixture]
+    public class QueueNuTests
+    {
+        [TestCase(0, 1, 2)]
+        public void CreateQueue(params int[] ints)
+        {
+
+        }
+
+    }
+}
